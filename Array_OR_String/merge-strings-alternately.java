31f9bd430bd5c2a6799a88397fc5636727c8d1b2
Atharva Girish Naik <138719165+AtharvaGNaik@users.noreply.github.com>
2024-01-01 23:06:04 +0530

    merge-strings-alternately.java

--- Array_OR_String/merge-strings-alternately.java
+++ Array_OR_String/merge-strings-alternately.java
@@ -1,17 +1,23 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder SB_result = new StringBuilder();
-        int i = 0, j = 0;
 
-        while (i < word1.length() || j < word2.length()) {
-            if (i < word1.length()) {
-                SB_result.append(word1.charAt(i));
-                i++;
-            }
-            if (j < word2.length()) {
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+
+        int j = 0;
+        for (int i = 0; i < word1Length; i++) {
+            SB_result.append(word1.charAt(i));
+
+            if (j < word2Length) {
                 SB_result.append(word2.charAt(j));
                 j++;
             }
+        }
+
+        while (j < word2Length) {
+            SB_result.append(word2.charAt(j));
+            j++;
         }
 
         return SB_result.toString();
