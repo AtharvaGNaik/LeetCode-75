ee9e70c2530ae98e6ed018870e7d837ab82a0d3f
Atharva Girish Naik <138719165+AtharvaGirishNaik@users.noreply.github.com>
2024-02-21 21:55:12 +0530

    Create Guess Number Higher or Lower.cpp

---
+++ Binary Search/Guess Number Higher or Lower.cpp
@@ -0,0 +1,44 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int s=1;
+        int e=n;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(guess(mid)==0){
+                return mid;
+            }
+            else if(guess(mid)==1){
+                s=mid+1;
+
+            }
+            else{
+                e=mid-1;
+            }
+
+        }
+        return -1;
+        
+    }
+}
+
+// Example 1:
+
+// Input: n = 10, pick = 6
+// Output: 6
+// Example 2:
+
+// Input: n = 1, pick = 1
+// Output: 1
+// Example 3:
+
+// Input: n = 2, pick = 1
+// Output: 1
